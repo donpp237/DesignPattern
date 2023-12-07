400db8c18f7ade0da4334008f3a71cc973c7e8aa
Gijung2 <137162873+donpp237@users.noreply.github.com>
2023-12-07 18:27:16 +0900

    Add New Pattern

---
+++ Behavior/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DesignPattern.Behavior.TemplateMethod
+{
+    /* [Template Method]
+    * 
+    * 1. 자식 클래스에서 재사용 할 수 있는 함수 선언.
+    * 2. 자식 클래스에서 함수를 오버라이딩 하여 재사용.
+    * 
+    */
+
+    #region Define
+
+    /// <summary>
+    /// Template Method Interface
+    /// </summary>
+    interface TemplateMethod
+    {
+        string ShowMessage();
+    }
+
+    #endregion
+
+    #region Using
+
+    /// <summary>
+    /// How to use Template Method?
+    /// </summary>
+    interface Country
+    {
+        string SayHello();
+    }
+
+    class Korea : Country
+    {
+        public string SayHello()
+        {
+            return "안녕";
+        }
+    }
+
+    class USA : Country 
+    {
+        public string SayHello()
+        {
+            return "HI";
+        }
+    }
+
+    /// <summary>
+    /// Example
+    /// </summary>
+    class Main
+    {
+        void ActualSituation()
+        {
+            // 국가별 인사를 출력
+            Country country = new Korea();
+            Console.WriteLine(country.SayHello());
+
+            country = new USA();
+            Console.WriteLine(country.SayHello());
+        }
+    }
+
+    #endregion
+
+}
---
+++ Creation/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,68 @@
+﻿namespace DesignPattern.Creation.FactoryMethod
+{
+    /* [Factory Method]
+   * 
+   * 1. 자식 클래스에서 객체를 생성 할 수 있도록 권한 위임.
+   * 
+   */
+
+    #region Define
+
+    /// <summary>
+    /// Factory Method Interface
+    /// </summary>
+    interface FactoryMethod
+    {
+        object Create();
+    }
+
+    #endregion
+
+    #region Using
+
+    /// <summary>
+    /// How to use Factory Method?
+    /// </summary>
+    interface Farm
+    {
+        Fruit Create();
+    }
+
+    class AppleFarm : Farm
+    {
+        public Fruit Create()
+        {
+            return new Pineapple();   
+        }
+    }
+
+    class PineappleFarm : Farm 
+    {
+        public Fruit Create()
+        {
+            return new Apple();
+        }
+    }
+
+    interface Fruit { }
+
+    class Apple : Fruit { }
+    class Pineapple : Fruit { }
+
+
+    /// <summary>
+    /// Example
+    /// </summary>
+    class Main
+    {
+        void ActualSituation()
+        {
+            // 농장(Farm)을 상속받은 자식 농장에서 무엇을 만들어 낼지는 자식만이 알수있다!
+            Farm farm = new AppleFarm();
+            Fruit fruit = farm.Create();
+        }
+    }
+    #endregion
+
+
+}
