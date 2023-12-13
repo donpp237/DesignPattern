15e9a262f2c2c4cf62417388e05668a030d48350
Gijung2 <137162873+donpp237@users.noreply.github.com>
2023-12-13 16:56:01 +0900

    Add Structure Patterns

---
+++ Structure/Adapter/Adapter.cs
@@ -0,0 +1,79 @@
+﻿namespace DesignPattern.Structure.Adapter
+{
+    /* [Adapter]
+     * 
+     * 1. 내부 코드 수정이 어려운 라이브러리를 원하는 형태, 기능으로 변경하고 싶을때 사용.
+     * 2. 라이브러리를 멤버로 갖는 경우 객체 어댑터, 상속을 받는 경우 클래스 어댑터라 지칭.
+     * 3. Wrapper라고도 칭함.
+     * 
+     */
+
+    #region Define
+    #endregion
+
+    #region Using
+
+    /// <summary>
+    /// How to use Adaptor?
+    /// </summary>
+    interface Usb 
+    {
+        string Using();
+    }
+
+    /// <summary>
+    /// Object Adapter
+    /// </summary>
+    class UsbAToCTpyeChanger_objectAdapter : Usb
+    {
+        USBAType m_adaptee;
+
+        public UsbAToCTpyeChanger_objectAdapter(USBAType adaptee) 
+        {
+            m_adaptee = adaptee;
+        }
+
+        public string Using()
+        {
+            return $"Converting Complete! [ {m_adaptee.Using()} -> C Type USB";
+        }
+    }
+
+    /// <summary>
+    /// Class Adapter
+    /// </summary>
+    class UsbAToCTpyeChanger_classAdapter : USBAType , Usb
+    {
+        public string Using()
+        {
+            return $"Converting Complete! [ {base.Using()} -> C Type USB";
+        }
+    }
+
+    /// <summary>
+    /// Legacy Adaptee
+    /// </summary>
+    class USBAType 
+    {
+        public string Using() { return "A Type USB"; }
+    }
+
+    /// <summary>
+    /// Example
+    /// </summary>
+    class Main
+    {
+        void ActualSituation()
+        {
+            // 객체 어댑터
+            Usb usb = new UsbAToCTpyeChanger_objectAdapter(new USBAType());
+            usb.Using();
+
+            // 클래스 어댑터
+            usb = new UsbAToCTpyeChanger_classAdapter();
+            usb.Using();
+        }
+    }
+
+    #endregion
+}
---
+++ Structure/Bridge/Bridge.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace DesignPattern.Structure.Bridge
+{
+    /* [Bridge]
+    * 
+    * 1. 객체를 표현하는 클래스와 
+    *    객체의 기능을 표현하는 클래스 모두 종류를 늘리고 싶을때 사용.
+    * 
+    */
+
+    #region Define
+
+    /// <summary>
+    /// Bridge Main Interface
+    /// </summary>
+    interface Instance
+    {
+        Function Function { get; }
+    }
+
+    /// <summary>
+    /// Bridge Sub Interface
+    /// </summary>
+    interface Function {}
+
+    #endregion
+
+    #region Using
+
+    /// <summary>
+    /// How to use Bridge Main Interface?
+    /// </summary>
+    interface Fruit
+    {
+        string Name { get; }
+        Color Color { get; set; }
+    }
+
+    class Apple : Fruit
+    {
+        public string Name { get; } = "Apple";
+        public Color Color { get; set; }
+    }
+
+    class Banana : Fruit
+    {
+        public string Name { get; } = "Banana";
+        public Color Color { get; set; }
+    }
+
+    /// <summary>
+    /// How to use Bridge Sub Interface?
+    /// </summary>
+    interface Color
+    {
+        string Name { get; }
+    }
+
+    class Red : Color
+    {
+        public string Name => "Red";
+    }
+
+    class Green : Color
+    {
+        public string Name => "Green";
+    }
+
+    /// <summary>
+    /// Example
+    /// </summary>
+    class Main
+    {
+        void ActualSituation()
+        {
+            // 사과 실체화
+            Fruit fruit = new Apple();
+            Console.WriteLine($"Q. What is Fruit?, A. {fruit.Name}");
+
+            // 사과의 색상 할당
+            fruit.Color = new Red();
+            Console.WriteLine($"Q. What is Fruit Color ?, A. {fruit.Color.Name}");
+
+
+            // 바나나 실체화
+            fruit = new Banana();
+            Console.WriteLine($"Q. What is Fruit?, A. {fruit.Name}");
+
+            // 바나나의 색상 할당
+            fruit.Color = new Green();
+            Console.WriteLine($"Q. What is Fruit Color ?, A. {fruit.Color.Name}");
+        }
+    }
+
+    #endregion
+}
